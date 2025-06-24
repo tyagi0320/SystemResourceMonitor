8e8a0ce77459218a08ff5133070fcb532ad40344
Harshit Tyagi <tharshit03@gmail.com>
2025-06-24 23:39:49 +0530

    updated code

--- setup/setup-server.sh
+++ setup/setup-server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Log output to file and console for debugging
-exec > >(tee /var/log/setup-script.log | logger -t user-data -s 2>/dev/console) 2>&1
-
 echo "[INFO] Updating package list..."
 apt update -y
 
